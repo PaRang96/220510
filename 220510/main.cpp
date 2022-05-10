171ce1b5eccf662922b6f0e686b660b05de76595
papasmf1 <papasmf1@gmail.com>
2022-05-10 09:49:02 +0900

    초기 커밋.

---
+++ 220510/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int ArrayAdd(int* list, int b)
+{
+	int result = 0;
+
+	for (int i = 0; i < b; i++)
+	{
+		result += list[i];
+	}
+
+	return result;
+}
+
+//void ArraySort(int list[], int n)
+//{
+//	for (int i = 0; i < i++)
+//}
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+
+	int* arr = new int[n];
+
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cout << ArrayAdd(arr,n);
+
+	//ArraySort(arr, n);
+
+	/*for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << endl;
+	}*/
+
+	delete[] arr;
+	return 0;
+}
